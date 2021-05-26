4c1b7db8f6b4c02b9547d27a534606bad901224e
James Han <jameshan20024@gmail.com>
2021-05-26 15:29:08 -0500

    Library Draft
    Fixed html and CSS issue. added javascript to work as it should.

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const popUpForm = document.getElementById("popup");
 const newBtn = document.getElementById("newBtn");
 newBtn.addEventListener('click', () => popUpForm.style.display = 'block');
 
+const addBtn = document.getElementById("addBtn");
+addBtn.addEventListener('click', addBookToLibrary);
+
 // Get the <span> element that closes the popup
 const closePopUp = document.getElementsByClassName("close")[0];
 closePopUp.addEventListener('click', () => popUpForm.style.display = 'none');
@@ -18,8 +21,101 @@ class Book {
 }
 
 let myLibrary = [];
-
-
+let newBook;
 function addBookToLibrary() {
-  // do stuff here
+    if (title.value.length === 0 || author.value.length === 0) {
+        alert("Please, fill all the fields");
+        event.preventDefault();
+        return;
+      }
+      else if (isNaN(pages.value)) 
+      {
+        alert("Your \"pages\" input should be in numbers.");
+        event.preventDefault();
+        return;
+      }
+
+    popUpForm.style.display = 'none';
+    event.preventDefault();
+    newBook = new Book(title, author, pages, read); 
+    myLibrary.push(newBook); 
+    updateLocalStorage();
+    render();
+    form.reset();   
 }
+
+function updateLocalStorage() {
+    localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+}
+
+function render()
+{
+    myLibrary = JSON.parse(localStorage.getItem("myLibrary"));
+
+    const display = document.getElementById('Library-container');
+    const books = document.querySelectorAll('.book');
+    books.forEach(book => display.removeChild(book));
+   
+    for (let i=0; i<myLibrary.length; i++){
+       createBook(myLibrary[i]);
+    }
+    
+}
+
+function createBook(item) {
+    const library = document.getElementById("Library-container");
+    const bookDiv = document.createElement('div');
+    const titleDiv = document.createElement('div');
+    const authDiv = document.createElement('div');
+    const pageDiv = document.createElement('div');
+    const removeBtn = document.createElement('button');
+    const readBtn = document.createElement('button');
+    
+    
+    bookDiv.classList.add('book');
+    bookDiv.setAttribute('id', myLibrary.indexOf(item));
+
+    titleDiv.textContent = item.title;
+    titleDiv.classList.add('title');
+    bookDiv.appendChild(titleDiv);
+
+    authDiv.textContent = item.author;
+    authDiv.classList.add('author');
+    bookDiv.appendChild(authDiv);
+
+    pageDiv.textContent = item.pages;
+    pageDiv.classList.add('pages');
+    bookDiv.appendChild(pageDiv);
+
+    readBtn.classList.add('readBtn');    
+    bookDiv.appendChild(readBtn);
+
+    if(item.read===false) {
+        readBtn.textContent = 'Not Read';
+        readBtn.style.backgroundColor = '#FF4136';
+    }else {
+        readBtn.textContent = 'Read';
+        readBtn.style.backgroundColor = '#2ECC40'
+    }
+
+    removeBtn.textContent = 'Remove'; 
+    removeBtn.setAttribute('id', 'removeBtn');
+    bookDiv.appendChild(removeBtn);
+    
+    library.appendChild(bookDiv);
+
+    removeBtn.addEventListener('click', () => {
+        myLibrary.splice(myLibrary.indexOf(item),1);
+        updateLocalStorage();
+        render();
+    });
+
+    //add toggle ability to each book 'read' button on click
+    readBtn.addEventListener('click', () => { 
+        item.read = !item.read; 
+        updateLocalStorage(); 
+        render();
+    }); 
+}
+
+render();
