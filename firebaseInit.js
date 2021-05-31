14fd0325e366fba32b1530bb1346e0d204517e5d
James Han <jameshan20024@gmail.com>
2021-05-31 11:06:06 -0500

    firebase init

---
+++ firebaseInit.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDaC3OBpErT5RUkzWJEm6VoKZmwtOPA7gQ",
+    authDomain: "top-library-661a4.firebaseapp.com",
+    projectId: "top-library-661a4",
+    storageBucket: "top-library-661a4.appspot.com",
+    messagingSenderId: "960028482911",
+    appId: "1:960028482911:web:1d71cb6e8ff451052b6aea",
+    measurementId: "G-PMSMBM6D03"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
